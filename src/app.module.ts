fe2773a189cab6d3f0c18ef7ea1b49600b1644ec
John Desjardins <desjjoh@gmail.com>
2025-11-03 23:08:18 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppConfigModule } from '@/modules/config/config.module';
 import { AppLoggerModule } from '@/modules/logger/logger.module';
 
 import { AppController } from '@/api/app/controllers/app.controller';
-
 import { AppService } from '@/api/app/services/app.service';
 
 @Module({
