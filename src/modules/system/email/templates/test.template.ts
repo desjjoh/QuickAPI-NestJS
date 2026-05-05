014bf4e7d881828324ee32ac23edf463209c022e
John Desjardins <3700168+desjjoh@users.noreply.github.com>
2026-05-05 16:07:46 -0400

    Update test.template.ts

--- src/modules/system/email/templates/test.template.ts
+++ src/modules/system/email/templates/test.template.ts
@@ -1,10 +1,5 @@
 import { EmailTemplate } from '../models/template.model';
 
-export type TestEmailTemplateModel = {
-  name: string;
-  message: string;
-};
-
 export const TestEmailTemplate = new EmailTemplate({
   key: 'generic-test-email',
   subject: 'Hello from Postmark',
