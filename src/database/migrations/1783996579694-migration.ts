aa001db8cbb414059f0399ec3d053a47b79ea912
John Desjardins <3700168+desjjoh@users.noreply.github.com>
2026-07-14 08:57:44 -0400

    Prettier Format

--- src/database/migrations/1783996579694-migration.ts
+++ src/database/migrations/1783996579694-migration.ts
@@ -1,20 +1,35 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1783996579694 implements MigrationInterface {
-    name = 'Migration1783996579694'
+  name = 'Migration1783996579694';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user_credentials\` ADD \`browser_version\` varchar(64) NULL`);
-        await queryRunner.query(`ALTER TABLE \`user_credentials\` ADD \`os_version\` varchar(64) NULL`);
-        await queryRunner.query(`ALTER TABLE \`user_credentials\` ADD \`origin\` varchar(2048) NULL`);
-        await queryRunner.query(`ALTER TABLE \`user_credentials\` ADD \`referrer\` varchar(2048) NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`user_credentials\` ADD \`browser_version\` varchar(64) NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`user_credentials\` ADD \`os_version\` varchar(64) NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`user_credentials\` ADD \`origin\` varchar(2048) NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`user_credentials\` ADD \`referrer\` varchar(2048) NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user_credentials\` DROP COLUMN \`referrer\``);
-        await queryRunner.query(`ALTER TABLE \`user_credentials\` DROP COLUMN \`origin\``);
-        await queryRunner.query(`ALTER TABLE \`user_credentials\` DROP COLUMN \`os_version\``);
-        await queryRunner.query(`ALTER TABLE \`user_credentials\` DROP COLUMN \`browser_version\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`user_credentials\` DROP COLUMN \`referrer\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`user_credentials\` DROP COLUMN \`origin\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`user_credentials\` DROP COLUMN \`os_version\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`user_credentials\` DROP COLUMN \`browser_version\``,
+    );
+  }
 }
--- src/database/migrations/1783996961336-migration.ts
+++ src/database/migrations/1783996961336-migration.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1783996961336 implements MigrationInterface {
-    name = 'Migration1783996961336'
+  name = 'Migration1783996961336';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user_credentials\` DROP COLUMN \`referrer\``);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`user_credentials\` DROP COLUMN \`referrer\``,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user_credentials\` ADD \`referrer\` varchar(2048) NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`user_credentials\` ADD \`referrer\` varchar(2048) NULL`,
+    );
+  }
 }
