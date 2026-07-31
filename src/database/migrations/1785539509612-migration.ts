e62b9c8781f986d02b438a2805bbe30e0f80bab0
John Desjardins <3700168+desjjoh@users.noreply.github.com>
2026-07-31 19:21:09 -0400

    Update 1785539509612-migration.ts

--- src/database/migrations/1785539509612-migration.ts
+++ src/database/migrations/1785539509612-migration.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1785539509612 implements MigrationInterface {
-    name = 'Migration1785539509612'
+  name = 'Migration1785539509612';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE UNIQUE INDEX \`UQ_activity_audits_event_operation\` ON \`activity_audits\` (\`event\`, \`operation_id\`)`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX \`UQ_activity_audits_event_operation\` ON \`activity_audits\` (\`event\`, \`operation_id\`)`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX \`UQ_activity_audits_event_operation\` ON \`activity_audits\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX \`UQ_activity_audits_event_operation\` ON \`activity_audits\``,
+    );
+  }
 }
